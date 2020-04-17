10ea9ec9d8afa60f8928ae368989cf7d9ed448b6
Abhishek Sharma <abhishek.sharma1@cars24.com>
2020-04-17 14:47:41 +0530

    Initial Commit

---
+++ springboot.basics/src/test/java/com/ichampin/springboot/basics/springboot/basics/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ichampin.springboot.basics.springboot.basics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
